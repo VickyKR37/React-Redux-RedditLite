5a5fc2bb42c995e78f40bd71a58f923eba273a95
VickyKR37 <67956865+VickyKR37@users.noreply.github.com>
2021-10-13 12:51:05 +0100

    refactored the slice into reduc reducer

--- src/components/searchResultsSlice.js
+++ src/components/searchResultsSlice.js
@@ -8,29 +8,17 @@ export const searchResults = {
         results: []
     },
 
-    const Content = {{initialState}} => {
-        if (!initialState.keywords && !initialState.fetching) {
-            return null
-        }
-
-        if (initialState.fetching) {
-            return <p>Searching for articles of {initialState.keywords}</p>
-        }
-
-        if (initialState.keywords.length) {
-            return (
-                <ul>
-                    {initialState.results.map(result =>(
-                        <li>{`https://www.reddit.com/r/${keyword}.json}`}</li>
-                    ))}
-                </ul>
-            )
-        }
-    }
-
     reducers: {
-        displaySearchResults: (state, action) => {
-            state.search[action.payload]
+
+        displayFetchingAlert: (state = initialState, action) => {
+            if (initialState.fetching) {
+                return <p>Searching for articles of {initialState.keywords}</p>
+        }
+
+        displaySearchResults: (state = initialState, action) => {
+            return initialState.results.map(result => (
+                <li>{`https://www.reddit.com/r/${keyword}.json}`}</li>
+            ))
         }
     }
 }
