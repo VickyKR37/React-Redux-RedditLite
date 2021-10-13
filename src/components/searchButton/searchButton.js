30a40158c498976cae60ac44a1cf24313b4c110a
VickyKR37 <67956865+VickyKR37@users.noreply.github.com>
2021-10-13 14:00:23 +0100

    created a searchButton folder and started functionality for the search button

---
+++ src/components/searchButton/searchButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './searchButton.css';
+import { searchResultsSlice } from './searchResults/searchResultsSlice.js';
+
+export default function searchButton ({keywords}) {
+    
+}
+
+
