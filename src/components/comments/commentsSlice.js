f7669603dad893e02bbd2ced3a2ce0b5c786d63f
VickyKR37 <67956865+VickyKR37@users.noreply.github.com>
2021-10-15 09:27:54 +0100

    completed commentsSlice.js

---
+++ src/components/comments/commentsSlice.js
@@ -0,0 +1,39 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const loadComments = createAsyncThunk(
+    'comments/loadComments',
+    async (commentsId) => {
+        const data = await fetch 
+        ('https://www.reddit.com/r/${commentsId}.json');
+            const json = await data.json();
+            return json;
+    }
+);
+
+export const commentsSlice = createSlice({
+    name: 'loadComments',
+    initialState: {
+        comments: undefined,
+        isLoadingComments: false,
+        hasError: false
+    },
+    extraReducers: (builder) => {
+        builder.addCase(loadComments.pending, (state) => {
+            state.isLoadingComments = true;
+            state.hasError = false;
+        })
+        .addCase(loadComments.fulfilled, (state, action) => {
+            state.isLoadingComments = false,
+            state.hasError = false;
+            state.comments = action.pauload;
+        })
+        .addCase(loadComments.rejected, (state) => {
+            state.isLoadingComments = false,
+            state.hasError = true,
+            state.comments = {};
+        })
+    },
+});
+
+
+export default commentsSlice.reducer;
