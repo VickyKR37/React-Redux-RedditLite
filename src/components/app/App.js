5cef3b03d004faccad0b8230dc6b2d3e170bdb7c
VickyKR37 <67956865+VickyKR37@users.noreply.github.com>
2021-11-02 10:48:37 +0000

    writing logic in app.js to display comments, transferred over from displayComments.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,11 +3,32 @@ import './App.css';
 import React from "react";
 import SearchBarAndButton from '../searchBarAndButton/searchBarAndButton';
 import { selectArticles } from '../searchBarAndButton/searchBarandButtonSlice';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { setPermalink, loadComments, selectComments, selectPermalink } from '../comments/commentsSlice';
+import Comments from '../comments/comments';
+
 
 
 export default function App() {
   const articles = useSelector(selectArticles);
+  const dispatch = useDispatch();
+  const comments = useSelector(selectComments);
+  const permalink = useSelector(selectPermalink);
+
+
+  function clickArticle(e) {
+    console.log(e.target);
+    dispatch(setPermalink(e.target.id));
+    dispatch(loadComments(e.target.id));
+  }
+
+  function handleSelectArticle(e) {
+    console.log(e, e.target.id)
+    dispatch(loadComments(permalink));
+    if (comments) {
+      return <Comments/>
+    }
+  }
   
   return (
     <div>
@@ -15,7 +36,21 @@ export default function App() {
       <h1>RedditLite</h1>
       <SearchBarAndButton class="button" />
       {articles.map(article => <p>{article.data.title}</p> )}
-      <displayComments/>
+      <Comments class="comments" />
+      if (permalink) {
+     return (
+     <div>
+       {comments.map((comment, index) => (<p key={index}>{comment.data.body}</p>))}
+     </div> )
+   }
+   else if (!articlesLoading && articles.length !== 0){
+     return (
+     <>
+       {articles.map((article, index) => (<p key={index} id={article.data.permalink} onClick={clickArticle}>{article.data.title}</p>) )}
+     </>
+  
+     )
+   }
     </div>
   </div>
        
--- src/components/comments/displayComments.js
+++ src/components/comments/displayComments.js
@@ -1,57 +1,32 @@
-import React from 'react';
-import SearchBarAndButton from '../searchBarAndButton/searchBarAndButton';
-import Comments from '../comments/comments';
-import { selectArticles } from '../searchBarAndButton/searchBarandButtonSlice';
-import { useSelector, useDispatch } from 'react-redux';
-import { isLoading } from '../searchBarAndButton/searchBarandButtonSlice';
-import { selectPermalink, setPermalink, loadComments, selectComments } from '../comments/commentsSlice';
+// import React from 'react';
+// import SearchBarAndButton from '../searchBarAndButton/searchBarAndButton';
+// import Comments from '../comments/comments';
+// import { selectArticles } from '../searchBarAndButton/searchBarandButtonSlice';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { isLoading } from '../searchBarAndButton/searchBarandButtonSlice';
 
-export default function DisplayComments() {
-  const articles = useSelector(selectArticles);
-  const articlesLoading = useSelector(isLoading);
-  const comments = useSelector(selectComments);
-  const dispatch = useDispatch();
-  const permalink = useSelector(selectPermalink);
+
+// export default function DisplayComments() {
+//   const articles = useSelector(selectArticles);
+//   const articlesLoading = useSelector(isLoading);
+//   const permalink = useSelector(selectPermalink);
   
   
-  if (permalink) {
-    return (
-    <div>
-      {comments.map((comment, index) => (<p key={index}>{comment.data.body}</p>))}
-    </div> )
-  }
-  else if (!articlesLoading && articles.length !== 0){
-    return (
-    <>
-      {articles.map((article, index) => (<p key={index} id={article.data.permalink} onClick={clickArticle}>{article.data.title}</p>) )}
-    </>
-  
-    )
-  }
+
     
 
-  function clickArticle(e) {
-    console.log(e.target);
-    dispatch(setPermalink(e.target.id));
-    dispatch(loadComments(e.target.id));
-  }
+  
 
-   function handleSelectArticle(e) {
-     console.log(e, e.target.id)
-     dispatch(loadComments(permalink));
-     if (comments) {
-       return <Comments/>
-     }
-   }
+ 
 
-   return (
-      <div>
-      <p>
-      {articlesLoading === true ? <p>Getting articles...</p> : 
-      articles.map((article, index) => <p id={article.data.permalink} key={index}
-      onClick={handleSelectArticle}>{article.data.title}</p>)}
-      </p>
-      </div>
-   )
+//    return (
+//       <div>
+//       <p>
+//       {articlesLoading === true ? <p>Getting articles...</p> : 
+//       articles.map((article, index) => <p id={article.data.permalink} key={index}
+//       onClick={handleSelectArticle}>{article.data.title}</p>)}
+//       </p>
+//       </div>
+//    )
 
-}
+// }
