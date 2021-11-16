dcb225097d654d542e15532b91a712f121dfe665
VickyKR37 <67956865+VickyKR37@users.noreply.github.com>
2021-11-16 16:18:29 +0000

    deleted article folder because it's not needed

--- src/components/searchBarAndButton/searchBarAndButton.js
+++ src/components/searchBarAndButton/searchBarAndButton.js
@@ -2,15 +2,11 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { searchForArticles, setKeyword } from "./searchBarandButtonSlice";
 import { clearResults } from '../comments/commentsSlice';
-// import { useForm } from "react-hook-form";
-
-
 
 
 
 export const SearchBarAndButton = ({searchButton}) => {
   const dispatch = useDispatch();
-  // const reset = useForm();
   
   function handleSubmit(e) {
    e.preventDefault();
@@ -19,18 +15,13 @@ export const SearchBarAndButton = ({searchButton}) => {
    dispatch(clearResults());
    dispatch(searchForArticles(keyword));
   }
-  // handleReset = () => {
-  //   this.setState({
-  //     itemvalues: [{}]
-  //   });
-  // };
+ 
 
   return (
     <div>
     <form onSubmit={ handleSubmit }>
       <input type="text" name="search" />
       <button type="submit">SEARCH RedditLite</button>
-      {/* <button type="button" onClick={() => reset("")}>New Search</button> */}
     </form>
     </div>
   );
