cbc8fcd6fd063694a4a4ca7230d18aee7aecba43
VickyKR37 <67956865+VickyKR37@users.noreply.github.com>
2021-11-17 11:37:20 +0000

    added 'votes' to displayed info (just in the ui, not functionality)

--- src/components/comments/comments.js
+++ src/components/comments/comments.js
@@ -19,6 +19,7 @@ import './comments.css';
                     <h6>Likes</h6>
                     <h6>User</h6>
                     <h6>Points and Comments</h6>
+                    <h6>Votes</h6>
                     <h6>Users possible actions</h6>>
                 </div>
             )
@@ -39,6 +40,7 @@ import './comments.css';
                     <h6>Likes</h6>
                     <h6>User</h6>
                     <h6>Points and Comments</h6>
+                    <h6>Votes</h6>
                     <h6>Users possible actions</h6>>
                 </div>
             )
