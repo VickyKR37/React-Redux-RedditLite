8453f96289cd2d882a4ec4f577d3add3ad7f2895
VickyKR37 <67956865+VickyKR37@users.noreply.github.com>
2021-11-19 12:29:10 +0000

    completed the code for the extra info to display and also changed the post images into thumbnails for initial rendering after search

--- src/components/comments/comments.js
+++ src/components/comments/comments.js
@@ -1,76 +1,95 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { setPermalink, loadComments, selectComments, selectPermalink } from '../comments/commentsSlice';
-import { selectArticles } from '../searchBarAndButton/searchBarandButtonSlice';
-import './comments.css';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  setPermalink,
+  loadComments,
+  selectComments,
+  selectPermalink,
+} from "../comments/commentsSlice";
+import { selectArticles } from "../searchBarAndButton/searchBarandButtonSlice";
+import "./comments.css";
 
-    export default function Comments() {
-        const articles = useSelector(selectArticles);
-        const dispatch = useDispatch();
-        const comments = useSelector(selectComments);
-        let permalink = useSelector(selectPermalink);
+export default function Comments() {
+  const articles = useSelector(selectArticles);
+  const dispatch = useDispatch();
+  const comments = useSelector(selectComments);
+  let permalink = useSelector(selectPermalink);
 
-        // function getTime() {
-        //     let time = article.data.created;
-        //     return time.toLocaleString();
-        // }
+  // function getTime(article) {
+  //     let time = article.data.created;
+  //     return time.toLocaleString();
+  // }
 
-        function displayExtraInfo() {
-            const article = articles.find( article => article.data.permalink === permalink);
-                return (
-                    <div>
-                    <h5>{article}</h5>
-                    <h6>Posted By: {article.data.author}</h6>
-                    {/* <h6>Time posted: {getTime}</h6> */}
-                    <h6>Subreddit: {article.data.display_name}</h6>
-                    <h6>Comments: {article.data.num_comments}</h6>
-                    <h6>Up Votes: {article.data.ups}</h6>
-                    <h6>Down Votes: {article.data.downs}</h6>
-                    </div>
-                )   
-        }
+  function displayExtraInfo() {
+    const article = articles.find(
+      (article) => article.data.permalink === permalink
+    );
+    return (
+      <div>
+        <h5>{article.data.title}</h5>
+        <h6>Posted By: {article.data.author}</h6>
+        <img className="article-img" src={article.data.url} />
+        {/* <h6>Time posted: {getTime}</h6> */}
+        <h6>Subreddit: {article.data.display_name}</h6>
+        <h6>Comments: {article.data.num_comments}</h6>
+        <h6>Up Votes: {article.data.ups}</h6>
+        <h6>Down Votes: {article.data.downs}</h6>
+        {comments.map((comment, i) => (
+          <p key={i} class="results">
+            {" "}
+            <span className="commentAuthor">{comment.data.author}</span>
+            {comment.data.body}{" "}
+          </p>
+        ))}
+      </div>
+    );
+  }
 
-        function clickArticle(e) {
-            console.log(e.target);
-            dispatch(setPermalink(e.target.closest(".article").id ));
-            dispatch(loadComments(e.target.closest(".article").id ));           
-          }
+  function clickArticle(e) {
+    console.log(e.target);
+    dispatch(setPermalink(e.target.closest(".article").id));
+    dispatch(loadComments(e.target.closest(".article").id));
+  }
 
-
-          function headline() {
-              if (comments.length == 0 && articles.length > 0) {
-                  return <h4 class="headline">Hi! Click on a headline for a list of comments associated with the article!</h4>
-              } 
-          }
-
-          function ArticleView(article, index) {
-            // returns an article view
-            return (
-                <div class="article" key={index} id={article.data.permalink} onClick={clickArticle, displayExtraInfo} >
-                    <h5>{article.data.title}</h5>
-                    <img class="article-img" src={article.data.url}/>
-                    <h6>Posted By: {article.data.author}</h6>
-                    {/* <h6>Time posted: {getTime}</h6> */}
-                    <h6>Subreddit: {article.data.display_name}</h6>
-                    <h6>Comments: {article.data.num_comments}</h6>
-                    <h6>Up Votes: {article.data.ups}</h6>
-                    <h6>Down Votes: {article.data.downs}</h6>
-                    <h6>Users possible actions</h6>
-                </div>
-            )
-          }
-
-        return (
-            <div>
-            <div class="headline">
-            {headline()}
-            </div>
-            <div id="container">
-            {comments.length > 0 ? comments.map((comment, i) => (<p key={i} class="results"> {comment.data.body} {comment.data.author} </p>)) :
-             articles.map(ArticleView) }
-           
-            </div>
-            </div>
-        )
+  function headline() {
+    if (comments.length == 0 && articles.length > 0) {
+      return (
+        <h4 class="headline">
+          Hi! Click on a headline for a list of comments associated with the
+          article!
+        </h4>
+      );
     }
-    
+  }
+
+  function ArticleView(article, index) {
+    // returns an article view
+    return (
+      <div
+        class="article"
+        key={index}
+        id={article.data.permalink}
+        onClick={clickArticle}
+      >
+        <h5>{article.data.title}</h5>
+        <img class="thumbnail" src={article.data.thumbnail} />
+        <h6>Posted By: {article.data.author}</h6>
+        {/* <h6>Time posted: {getTime()}</h6> */}
+        <h6>Subreddit: {article.data.display_name}</h6>
+        <h6>Comments: {article.data.num_comments}</h6>
+        <h6>Up Votes: {article.data.ups}</h6>
+        <h6>Down Votes: {article.data.downs}</h6>
+        <h6>Users possible actions</h6>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div class="headline">{headline()}</div>
+      <div id="container">
+        {comments.length > 0 ? displayExtraInfo() : articles.map(ArticleView)}
+      </div>
+    </div>
+  );
+}
