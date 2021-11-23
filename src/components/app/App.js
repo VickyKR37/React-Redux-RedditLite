06f0f7c6cf9225a1c3318a715421d709228a2857
VickyKR37 <67956865+VickyKR37@users.noreply.github.com>
2021-11-23 11:57:25 +0000

    started the code for react-router to add functionality to the browsers in-built back and forward arrow buttons

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,32 +1,22 @@
-
-import './App.css';
+import "./App.css";
 import React from "react";
-import SearchBarAndButton from '../searchBarAndButton/searchBarAndButton';
-//import { selectArticles } from '../searchBarAndButton/searchBarandButtonSlice';
-import { useSelector } from 'react-redux';
-import { selectPermalink } from '../comments/commentsSlice';
-import Comments from '../comments/comments';
- 
-
-
+import SearchBarAndButton from "../searchBarAndButton/searchBarAndButton";
+import Comments from "../comments/comments";
 
 export default function App() {
-  //const articles = useSelector(selectArticles);
-  //const dispatch = useDispatch();
-  //const comments = useSelector(selectComments);
-  let permalink = useSelector(selectPermalink);
-
-  
   return (
     <div>
-    <div>
-      <h1>RedditLite</h1>
-      <SearchBarAndButton class="button" />
-      <Comments />
+      <div>
+        <h1>RedditLite</h1>
+        <SearchBarAndButton class="button" />
+        <Comments />
+      </div>
+      <div>
+        <img class="logo" src={"reddit-logo.png"} alt="Reddit Logo" />
+      </div>
+      <div>
+        <main />
+      </div>
     </div>
-    <div>
-      <img class="logo" src={'reddit-logo.png'} alt="Reddit Logo"/>
-    </div>
-  </div> 
   );
 }
