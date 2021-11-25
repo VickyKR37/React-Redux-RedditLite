8b28403e0fb69d042f62ab768fc529ea76827246
VickyKR37 <67956865+VickyKR37@users.noreply.github.com>
2021-11-25 15:30:08 +0000

    created a new folder to handle the search results in order to make routing easier. Also, added code to try to display the container only when search results appear

--- src/components/searchBarAndButton/searchBarAndButton.js
+++ src/components/searchBarAndButton/searchBarAndButton.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React from "react";
+import { useDispatch } from "react-redux";
 import { searchForArticles, setKeyword } from "./searchBarandButtonSlice";
-import { clearResults } from '../comments/commentsSlice';
+import { clearResults } from "../comments/commentsSlice";
 
-
-
-export const SearchBarAndButton = ({searchButton}) => {
+export const SearchBarAndButton = ({ searchButton }) => {
   const dispatch = useDispatch();
-  
+
   function handleSubmit(e) {
-   e.preventDefault();
-   const keyword = document.getElementsByName("search")[0].value;
-   dispatch(setKeyword(keyword));
-   dispatch(clearResults());
-   dispatch(searchForArticles(keyword));
+    e.preventDefault();
+    const keyword = document.getElementsByName("search")[0].value;
+    dispatch(setKeyword(keyword));
+    dispatch(clearResults());
+    dispatch(searchForArticles(keyword));
   }
- 
+
+  function displayContainer(e) {
+    e.preventDefault();
+    var border = document.getElementById("container");
+    return {
+      border.style.display = 'block';
+  }
+  };
 
   return (
     <div>
-    <form onSubmit={ handleSubmit }>
-      <input type="text" name="search" />
-      <button type="submit">SEARCH RedditLite</button>
-    </form>
+      <form onSubmit={handleSubmit, displayContainer}>
+        <input type="text" name="search" />
+        <button type="submit">SEARCH RedditLite</button>
+      </form>
     </div>
   );
 };
